f49492d1f24466fcb489a8bd9617cb7f88b1f3b9
nnavas <n_navas@hotmail.com>
2026-06-14 14:44:59 -0500

    Committed changes to create 40 peers (org4 with 10 peers).

--- test-network/scripts/deployCC.sh
+++ test-network/scripts/deployCC.sh
@@ -94,6 +94,11 @@ for (( PEER=0; PEER<=9; PEER++ )); do
   installChaincode2 3 ${PEER}
 done
 
+for (( PEER=0; PEER<=9; PEER++ )); do
+  warnln "NN: Installing chaincode on peer${PEER}.org3..."
+  installChaincode2 4 ${PEER}
+done
+
 resolveSequence
 
 ## query whether the chaincode is installed
@@ -104,39 +109,53 @@ approveForMyOrg 1
 
 ## check whether the chaincode definition is ready to be committed
 ## expect org1 to have approved and org2 not to
-checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": false" "\"Org3MSP\": false"
-checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": false" "\"Org3MSP\": false"
+checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": false" "\"Org3MSP\": false" "\"Org4MSP\": false"
+checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": false" "\"Org3MSP\": false" "\"Org4MSP\": false"
+checkCommitReadiness 3 "\"Org1MSP\": true" "\"Org2MSP\": false" "\"Org3MSP\": false" "\"Org4MSP\": false"
+checkCommitReadiness 4 "\"Org1MSP\": true" "\"Org2MSP\": false" "\"Org3MSP\": false" "\"Org4MSP\": false"
 
 ## now approve also for org2
 approveForMyOrg 2
 
 ## check whether the chaincode definition is ready to be committed
 ## expect them both to have approved
-checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": false"
-checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": false"
+checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": false" "\"Org4MSP\": false"
+checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": false" "\"Org4MSP\": false"
+checkCommitReadiness 3 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": false" "\"Org4MSP\": false"
+checkCommitReadiness 4 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": false" "\"Org4MSP\": false"
 
 ## now approve also for org3
 approveForMyOrg 3
 
 ## check whether the chaincode definition is ready to be committed
-## expect them both to have approved
-checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true"
-checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true"
+checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true" "\"Org4MSP\": false"
+checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true" "\"Org4MSP\": false"
+checkCommitReadiness 3 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true" "\"Org4MSP\": false"
+checkCommitReadiness 4 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true" "\"Org4MSP\": false"
+
+## now approve also for org3
+approveForMyOrg 4
+
+## check whether the chaincode definition is ready to be committed
+checkCommitReadiness 1 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true" "\"Org4MSP\": true"
+checkCommitReadiness 2 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true" "\"Org4MSP\": true"
+checkCommitReadiness 3 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true" "\"Org4MSP\": true"
+checkCommitReadiness 4 "\"Org1MSP\": true" "\"Org2MSP\": true" "\"Org3MSP\": true" "\"Org4MSP\": true"
 
 ## now that we know for sure all orgs have approved, commit the definition
-commitChaincodeDefinition 1 2 3
+commitChaincodeDefinition 1 2 3 4
 
 ## query on both orgs to see that the definition committed successfully
 queryCommitted 1
 queryCommitted 2
 queryCommitted 3
-
+queryCommitted 4
 ## Invoke the chaincode - this does require that the chaincode have the 'initLedger'
 ## method defined
 if [ "$CC_INIT_FCN" = "NA" ]; then
   infoln "Chaincode initialization is not required"
 else
-  chaincodeInvokeInit 1 2 3
+  chaincodeInvokeInit 1 2 3 4
 fi
 
 exit 0
--- test-network/scripts/envVar.sh
+++ test-network/scripts/envVar.sh
@@ -77,102 +77,102 @@ setGlobals2() {
     export CORE_PEER_LOCALMSPID=Org1MSP
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${TEST_NETWORK_HOME}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-	if [ $PEER -eq 0 ]; then
-      export CORE_PEER_ADDRESS=localhost:7051
-	elif [ $PEER -eq 1 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10001
-	elif [ $PEER -eq 2 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10011  
-	elif [ $PEER -eq 3 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10021  
-	elif [ $PEER -eq 4 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10031    
-	elif [ $PEER -eq 5 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10041  
-	elif [ $PEER -eq 6 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10051  
-	elif [ $PEER -eq 7 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10061  
-	elif [ $PEER -eq 8 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10071  
-	elif [ $PEER -eq 9 ]; then
-	  export CORE_PEER_ADDRESS=localhost:10081  
-	fi
+    if [ $PEER -eq 0 ]; then
+        export CORE_PEER_ADDRESS=localhost:7051
+    elif [ $PEER -eq 1 ]; then
+      export CORE_PEER_ADDRESS=localhost:10001
+    elif [ $PEER -eq 2 ]; then
+      export CORE_PEER_ADDRESS=localhost:10011  
+    elif [ $PEER -eq 3 ]; then
+      export CORE_PEER_ADDRESS=localhost:10021  
+    elif [ $PEER -eq 4 ]; then
+      export CORE_PEER_ADDRESS=localhost:10031    
+    elif [ $PEER -eq 5 ]; then
+      export CORE_PEER_ADDRESS=localhost:10041  
+    elif [ $PEER -eq 6 ]; then
+      export CORE_PEER_ADDRESS=localhost:10051  
+    elif [ $PEER -eq 7 ]; then
+      export CORE_PEER_ADDRESS=localhost:10061  
+    elif [ $PEER -eq 8 ]; then
+      export CORE_PEER_ADDRESS=localhost:10071  
+    elif [ $PEER -eq 9 ]; then
+      export CORE_PEER_ADDRESS=localhost:10081  
+    fi
   elif [ $USING_ORG -eq 2 ]; then
     export CORE_PEER_LOCALMSPID=Org2MSP
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
     export CORE_PEER_MSPCONFIGPATH=${TEST_NETWORK_HOME}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-	if [ $PEER -eq 0 ]; then
-      export CORE_PEER_ADDRESS=localhost:9051
-	elif [ $PEER -eq 1 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20001
-	elif [ $PEER -eq 2 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20011  
-	elif [ $PEER -eq 3 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20021  
-	elif [ $PEER -eq 4 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20031    
-	elif [ $PEER -eq 5 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20041  
-	elif [ $PEER -eq 6 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20051  
-	elif [ $PEER -eq 7 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20061  
-	elif [ $PEER -eq 8 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20071  
-	elif [ $PEER -eq 9 ]; then
-	  export CORE_PEER_ADDRESS=localhost:20081
-	fi
+    if [ $PEER -eq 0 ]; then
+        export CORE_PEER_ADDRESS=localhost:9051
+    elif [ $PEER -eq 1 ]; then
+      export CORE_PEER_ADDRESS=localhost:20001
+    elif [ $PEER -eq 2 ]; then
+      export CORE_PEER_ADDRESS=localhost:20011  
+    elif [ $PEER -eq 3 ]; then
+      export CORE_PEER_ADDRESS=localhost:20021  
+    elif [ $PEER -eq 4 ]; then
+      export CORE_PEER_ADDRESS=localhost:20031    
+    elif [ $PEER -eq 5 ]; then
+      export CORE_PEER_ADDRESS=localhost:20041  
+    elif [ $PEER -eq 6 ]; then
+      export CORE_PEER_ADDRESS=localhost:20051  
+    elif [ $PEER -eq 7 ]; then
+      export CORE_PEER_ADDRESS=localhost:20061  
+    elif [ $PEER -eq 8 ]; then
+      export CORE_PEER_ADDRESS=localhost:20071  
+    elif [ $PEER -eq 9 ]; then
+      export CORE_PEER_ADDRESS=localhost:20081
+    fi
   elif [ $USING_ORG -eq 3 ]; then
     export CORE_PEER_LOCALMSPID=Org3MSP
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG3_CA
     export CORE_PEER_MSPCONFIGPATH=${TEST_NETWORK_HOME}/organizations/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
-	if [ $PEER -eq 0 ]; then
-      export CORE_PEER_ADDRESS=localhost:30001
-  elif [ $PEER -eq 1 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30011
-	elif [ $PEER -eq 2 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30021  
-	elif [ $PEER -eq 3 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30031  
-	elif [ $PEER -eq 4 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30041    
-	elif [ $PEER -eq 5 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30051  
-	elif [ $PEER -eq 6 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30061  
-	elif [ $PEER -eq 7 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30071  
-	elif [ $PEER -eq 8 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30081  
-	elif [ $PEER -eq 9 ]; then
-	  export CORE_PEER_ADDRESS=localhost:30091
-	fi
+    if [ $PEER -eq 0 ]; then
+        export CORE_PEER_ADDRESS=localhost:30001
+    elif [ $PEER -eq 1 ]; then
+      export CORE_PEER_ADDRESS=localhost:30011
+    elif [ $PEER -eq 2 ]; then
+      export CORE_PEER_ADDRESS=localhost:30021  
+    elif [ $PEER -eq 3 ]; then
+      export CORE_PEER_ADDRESS=localhost:30031  
+    elif [ $PEER -eq 4 ]; then
+      export CORE_PEER_ADDRESS=localhost:30041    
+    elif [ $PEER -eq 5 ]; then
+      export CORE_PEER_ADDRESS=localhost:30051  
+    elif [ $PEER -eq 6 ]; then
+      export CORE_PEER_ADDRESS=localhost:30061  
+    elif [ $PEER -eq 7 ]; then
+      export CORE_PEER_ADDRESS=localhost:30071  
+    elif [ $PEER -eq 8 ]; then
+      export CORE_PEER_ADDRESS=localhost:30081  
+    elif [ $PEER -eq 9 ]; then
+      export CORE_PEER_ADDRESS=localhost:30091
+    fi
   elif [ $USING_ORG -eq 4 ]; then
     export CORE_PEER_LOCALMSPID=Org4MSP
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG4_CA
     export CORE_PEER_MSPCONFIGPATH=${TEST_NETWORK_HOME}/organizations/peerOrganizations/org4.example.com/users/Admin@org4.example.com/msp
-	if [ $PEER -eq 0 ]; then
-      export CORE_PEER_ADDRESS=localhost:40001
-  elif [ $PEER -eq 1 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40011
-	elif [ $PEER -eq 2 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40021  
-	elif [ $PEER -eq 3 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40031  
-	elif [ $PEER -eq 4 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40041    
-	elif [ $PEER -eq 5 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40051  
-	elif [ $PEER -eq 6 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40061  
-	elif [ $PEER -eq 7 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40071  
-	elif [ $PEER -eq 8 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40081  
-	elif [ $PEER -eq 9 ]; then
-	  export CORE_PEER_ADDRESS=localhost:40091
-	fi
+    if [ $PEER -eq 0 ]; then
+        export CORE_PEER_ADDRESS=localhost:40001
+    elif [ $PEER -eq 1 ]; then
+      export CORE_PEER_ADDRESS=localhost:40011
+    elif [ $PEER -eq 2 ]; then
+      export CORE_PEER_ADDRESS=localhost:40021  
+    elif [ $PEER -eq 3 ]; then
+      export CORE_PEER_ADDRESS=localhost:40031  
+    elif [ $PEER -eq 4 ]; then
+      export CORE_PEER_ADDRESS=localhost:40041    
+    elif [ $PEER -eq 5 ]; then
+      export CORE_PEER_ADDRESS=localhost:40051  
+    elif [ $PEER -eq 6 ]; then
+      export CORE_PEER_ADDRESS=localhost:40061  
+    elif [ $PEER -eq 7 ]; then
+      export CORE_PEER_ADDRESS=localhost:40071  
+    elif [ $PEER -eq 8 ]; then
+      export CORE_PEER_ADDRESS=localhost:40081  
+    elif [ $PEER -eq 9 ]; then
+      export CORE_PEER_ADDRESS=localhost:40091
+    fi
   else
     errorln "ORG Unknown"
   fi
